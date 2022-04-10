d5901e35162415f8bb8ac99beb0c9706fca423b8
100dongwoo <qorehddn123@naver.com>
2022-04-10 13:04:18 +0900

    add protect handler

--- libs/server/withHandler.ts
+++ libs/server/withHandler.ts
@@ -1,23 +1,35 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from 'next';
 
 export interface ResponseType {
-  ok: boolean;
-  [key: string]: any;
+    ok: boolean;
+    [key: string]: any;
+}
+interface ConfigType {
+    method: 'GET' | 'POST' | 'DELETE';
+    handler: (req: NextApiRequest, res: NextApiResponse) => void;
+    isPrivate?: boolean;
 }
 
-export default function withHandler(
-  method: "GET" | "POST" | "DELETE",
-  fn: (req: NextApiRequest, res: NextApiResponse) => void
-) {
-  return async function (req: NextApiRequest, res: NextApiResponse):Promise<any> {
-    if (req.method !== method) {
-      return res.status(405).end();
-    }
-    try {
-      await fn(req, res);
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({ error });
-    }
-  };
+export default function withHandler({
+    method,
+    isPrivate = true,
+    handler,
+}: ConfigType) {
+    return async function (
+        req: NextApiRequest,
+        res: NextApiResponse
+    ): Promise<any> {
+        if (req.method !== method) {
+            return res.status(405).end();
+        }
+        if (isPrivate && !req.session.user) {
+            return res.status(401).json({ ok: false, err: 'please login' });
+        }
+        try {
+            await handler(req, res);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ error });
+        }
+    };
 }
--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,71 +1,25 @@
-import twilio from 'twilio';
 import { NextApiRequest, NextApiResponse } from 'next';
 import withHandler, { ResponseType } from '@libs/server/withHandler';
 import client from '@libs/server/client';
-import mail from '@sendgrid/mail';
+import { withApiSession } from '@libs/server/withSession';
 
-mail.setApiKey(process.env.SENDGRID_API_KEY!);
-
-const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 async function handler(
     req: NextApiRequest,
     res: NextApiResponse<ResponseType>
 ) {
-    // + => string to int로 변경
-    // const user = await client.user.upsert({
-    //     //upsert 수정 or 생성시 사용
-    //     where: {
-    //         ...payload,
-    //     },
-    //     create: {
-    //         name: 'Anonymous',
-    //         ...payload,
-    //     },
-    //     update: {},
-    // });
-
-    const { phone, email } = req.body;
-    const user = phone ? { phone: +phone } : email ? { email } : null;
-    if (!user) {
-        return res.status(400).json({ ok: false });
-    }
-    const payload = Math.floor(100000 + Math.random() * 900000) + '';
-    const token = await client.token.create({
-        data: {
-            payload,
-            user: {
-                connectOrCreate: {
-                    where: {
-                        ...user,
-                    },
-                    create: {
-                        name: 'Anonymous',
-                        ...user,
-                    },
-                },
-            },
-        },
+    const profile = await client.user.findUnique({
+        where: { id: req.session.user?.id },
     });
-    if (phone) {
-        // const message = await twilioClient.messages.create({
-        //     messagingServiceSid: process.env.TWILIO_MSID,
-        //     to: process.env.MY_PHONE!,
-        //     body: `Your login token is ${payload}.`,
-        // });
-        // console.log(message);
-    } else if (email) {
-        // const email = await mail.send({
-        //     from: 'qorehddn123@naver.com',
-        //     to: 'qorehddn123@naver.com', //원래는 바디 데터
-        //     subject: 'Your carrot market verification email',
-        //     text: `your tkoen is ${payload}`,
-        //     html: `<strong>your token is ${payload}</strong>`,
-        // });
-        // console.log(email);
-    }
-    return res.json({
+    res.json({
         ok: true,
+        profile,
     });
 }
 
-export default withHandler('POST', handler);
+export default withApiSession(
+    withHandler({
+        method: 'GET',
+        handler,
+        isPrivate: false,
+    })
+);
